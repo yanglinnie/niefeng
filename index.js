1f846b482183c686abf424055b97332e887d2ac8
结婚的傻熊 <yanglinnie@126.com>
2015-02-11 15:58:00 +0800

    mongoskin

--- index.js
+++ index.js
@@ -14,4 +14,5 @@ exports.Request = require('request');
 exports.Xml2js = require('xml2js');
 exports.wechat=require('wechat') ;
 exports.wechatapi=require('wechat-api') ;
-exports.wechatoauth=require('wechatoauth') ;
+exports.wechatoauth=require('wechat-oauth') ;
+exports.mongoskin=require('mongoskin') ;
